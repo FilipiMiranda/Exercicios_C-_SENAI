67e2abbd0b84cc04d00472776bcd659ab0589e96
Filipi Henrique Miranda Benedito <filipi.benedito12@gmail.com>
2025-02-04 15:55:43 -0300

    Add files via upload

---
+++ Exercicio2PWBE/Exercicio2PWBE/Program.cs
@@ -0,0 +1,133 @@
+﻿// exercicio 1
+
+//int[] num = { 1, 12, 123, 1234, 12345, 123456, 1234567 };
+
+//foreach (int valor in num)
+//{
+//    Console.WriteLine(valor);
+//}
+
+// exercicio 2
+
+//for (int i = 1; i <= 7; i++)
+//{
+//    for (int j = 1; j <= i; j++)
+//    {
+//        Console.Write(j);  
+//    }
+//    for (int k = i; k < 7; k++)
+//    {
+//        Console.Write("*");  
+//    }
+//    Console.WriteLine(); 
+//}
+
+// exercicio 3
+
+//Console.Write("Informe um Número: ");
+//double num1 = double.Parse(Console.ReadLine());
+
+//Console.Write("Informe outro Número: ");
+//double num2 = double.Parse(Console.ReadLine());
+
+//if( num1 == 0)
+//{
+//    Console.WriteLine("Não é possivel dividir");   
+//}
+//else if (num2 == 0)
+//{
+//    Console.WriteLine("Não é possivel dividir");
+//}
+//else
+//{
+//double divisao = num1 / num2;
+
+//Console.WriteLine("Resultado: " + divisao);
+
+//}
+
+
+// exercicio 4
+
+//Console.Write("Digite um número: ");
+//if (int.TryParse(Console.ReadLine(), out int numero))
+//{
+//    for (int i = 1; i < numero; i += 2)
+//    {
+//        Console.WriteLine($"\nTabuada do {i}:");
+//        for (int j = 1; j <= 10; j++)
+//        {
+//            Console.WriteLine($"{i} x {j} = {i * j}");
+//        }
+//    }
+//}
+//else
+//{
+//    Console.WriteLine("Por favor, insira um número válido.");
+//}
+
+// exercicio 5
+
+//int idadeMaisVelha = 0;
+//double alturaMaisAlta = 0.0;
+
+//for (int i = 1; i <= 5; i++)
+//{
+//    Console.Write($"Digite a idade da pessoa {i}: ");
+//    int idade = int.Parse(Console.ReadLine());
+
+//    Console.Write($"Digite a altura da pessoa {i} (em metros): ");
+//    double altura = double.Parse(Console.ReadLine());
+
+//    if (idade > idadeMaisVelha)
+//    {
+//        idadeMaisVelha = idade;
+//    }
+
+//    if (altura > alturaMaisAlta)
+//    {
+//        alturaMaisAlta = altura;
+//    }
+//}
+
+//Console.WriteLine($"A idade da pessoa mais velha é: {idadeMaisVelha} anos");
+//Console.WriteLine($"A altura da pessoa mais alta é: {alturaMaisAlta} metros");
+
+// exercicio 6
+
+//Console.Write("Digite o número de alunos na sala: ");
+//int numAlunos = int.Parse(Console.ReadLine());
+
+//double somaNotas = 0.0;
+
+//for (int i = 1; i <= numAlunos; i++)
+//{
+//    Console.Write($"Digite a nota do aluno {i}: ");
+//    double nota = double.Parse(Console.ReadLine());
+//    somaNotas += nota;
+//}
+
+//double media = somaNotas / numAlunos;
+//Console.WriteLine($"A média da sala é: {media:F2}");
+
+// exercicio 7
+
+//Console.Write("Digite uma palavra: ");
+//string palavra = Console.ReadLine().ToLower().Replace(" ", "");
+
+//string reverso = "";
+//for (int i = palavra.Length - 1; i >= 0; i--)
+//{
+//    reverso += palavra[i];
+//}
+
+//if (palavra == reverso)
+//{
+//    Console.WriteLine("A palavra é um palíndromo.");
+//}
+//else
+//{
+//    Console.WriteLine("A palavra não é um palíndromo.");
+//}
+
+
---
+++ ExerciciosPWBE/ExerciciosPWBE/Program.cs
@@ -0,0 +1,170 @@
+﻿// exercicio 1
+
+//decimal precoMenos12 = 1.00m;
+//decimal precoMais12 = 1.30m;
+//decimal precoUnitario;
+
+//Console.Write("Qual é a quantidade de maçãs que irá querer: ");
+//int quantidade = int.Parse(Console.ReadLine());
+
+//if (quantidade <= 12)
+//{
+//    precoUnitario = precoMenos12;
+//}
+//else
+//{
+//    precoUnitario = precoMais12;
+//}
+
+//decimal total = quantidade * precoUnitario;
+
+//Console.WriteLine($"Preço unitário: R$ {precoUnitario:F2}");
+//Console.WriteLine($"Total a pagar: R$ {total:F2}");
+
+// exercicio 2
+
+
+//Console.Write("Informe um número: ");
+//int num1 = int.Parse(Console.ReadLine());
+
+//Console.Write("Informe outro número: ");
+//int num2 = int.Parse(Console.ReadLine());
+
+//Console.Write("Informe o ultimo número: ");
+//int num3 = int.Parse(Console.ReadLine());
+
+//int maior = Math.Max(num1, Math.Max(num2, num3));
+//int menor = Math.Min(num1, Math.Min(num2, num3));
+
+//int meio = 0;
+
+//if (num1 > num2 && num1 < num3)
+//{
+//    meio = num1;
+//}
+//else if (num2 > num1 && num2 < num3)
+//{
+//    meio = num2;
+//}
+//else if (num3 > num1 && num3 < num2)
+//{
+//    meio = num3;
+//}
+
+//Console.WriteLine($"Numero maior: {maior:F2}");
+//Console.WriteLine($"Numero do meio: {meio:F2}");
+//Console.WriteLine($"Numero menor: {menor:F2}");
+
+// exercicio 3
+
+//Console.Write("Qual é o seu salário: R$");
+//double salario = double.Parse(Console.ReadLine());
+
+//Console.Write("Quanto de Vendas você fez: R$");
+//int vendas = int.Parse(Console.ReadLine());
+
+//double comissao = 0;
+
+//if (vendas < 1500)
+//{
+//    comissao = vendas * 0.03;
+//}
+//else if (vendas >= 1500)
+//{
+//    comissao = vendas * 0.05;
+//}
+
+//double salarioTotal = comissao + salario;
+
+//Console.WriteLine("Valor da Comissão: R$" + comissao);
+//Console.WriteLine("Salário Total: R$" + salarioTotal);
+
+// exercicio 4
+
+
+// Solicita ao usuário que informe um ano
+//    Console.Write("Digite um ano: ");
+//    int ano = int.Parse(Console.ReadLine());
+
+//    Verifica se o ano é bissexto
+//    bool ehBissexto = (ano % 4 == 0 && ano % 100 != 0) || (ano % 400 == 0);
+
+//    Exibe o resultado
+//    if (ehBissexto)
+//    {
+//        Console.WriteLine($"O ano {ano} é bissexto.");
+//    }
+//    else
+//    {
+//        Console.WriteLine($"O ano {ano} não é bissexto.");
+//    }
+
+
+//    // exercicio 5
+//    Console.Write("Digite o preço normal da entrada: ");
+//    double precoNormal = double.Parse(Console.ReadLine());
+
+
+//    Console.Write("Digite o dia da semana (1 a 7): ");
+//    int diaSemana = int.Parse(Console.ReadLine());
+
+
+//    Console.Write("É dia de música ao vivo? (S/N): ");
+//    char musicaAoVivo = char.ToUpper(Console.ReadLine()[0]);
+
+
+//    double precoFinal = precoNormal;
+
+//    if (diaSemana == 2 || diaSemana == 3 || diaSemana == 5)
+//    {
+//        precoFinal -= 10;
+//    }
+
+//    if (musicaAoVivo == 'S')
+//    {
+//        precoFinal += 7.50;
+//    }
+
+//    Console.WriteLine($"O preço final da entrada é: R$ {precoFinal:F2}");
+
+////exercicio 6
+
+//    Console.Write("Digite o valor de A: ");
+//    double A = Convert.ToDouble(Console.ReadLine());
+
+//    Console.Write("Digite o valor de B: ");
+//    double B = Convert.ToDouble(Console.ReadLine());
+
+//    Console.Write("Digite o valor de C: ");
+//    double C = Convert.ToDouble(Console.ReadLine());
+
+//    if (EhTriangulo(A, B, C))
+//    {
+//        Console.WriteLine("Os valores informados formam um triângulo do tipo: " + TipoTriangulo(A, B, C));
+//    }
+//    else
+//    {
+//        Console.WriteLine("Os valores informados não formam um triângulo.");
+//    }
+
+
+//static bool EhTriangulo(double a, double b, double c)
+//{
+//    return a + b > c && a + c > b && b + c > a;
+//}
+
+//static string TipoTriangulo(double a, double b, double c)
+//{
+//    if (a == b && b == c)
+//    {
+//        return "Equilátero";
+//    }
+//    else if (a == b || a == c || b == c)
+//    {
+//        return "Isósceles";
+//    }
+//    else
+//    {
+//        return "Escaleno";
+//    }
+//}
